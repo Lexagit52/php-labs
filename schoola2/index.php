a707d7cd1c3454e3ccfb102ce813ef5c045ce3fa
Lexagit52 <lehakolomiez29@gmail.com>
2025-10-30 13:49:00 +0500

    lpr 9

--- schoola2/index.php
+++ schoola2/index.php
@@ -1,33 +1,33 @@
+
 <?php
+// Имя файла журнала
 define('PATH_LOG', 'log/path.log');
-
-
 include 'inc/log.inc.php';
 ?>
+
 
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="utf-8">
     <title>Сайт нашей школы</title>
-    <link rel="stylesheet" href="schoola2\inc\style.css">
+    <link rel="stylesheet" href="inc/style.css">
 </head>
 <body>
 
 <nav>
     <ul>
-
         <li><a href="index.php?id=about">О нас</a></li>
         <li><a href="index.php?id=contact">Контакты</a></li>
         <li><a href="index.php?id=info">Информация</a></li>
+        <li><a href="index.php?id=gbook">Гостевая книга</a></li>
+      <li><a href='index.php?id=log'>Журнал посещений</a></li>
 
-        <li><a href="index.php?id=log">Журнал посещений</a></li>
     </ul>
 </nav>
 
 <main>
 <?php
-// Вывод содержимого страниц
 $id = $_GET['id'] ?? 'home';
 switch($id){
     case 'about':
@@ -39,8 +39,8 @@ switch($id){
     case 'info':
         include 'inc/info.inc.php';
         break;
-    case 'guest':
-        include 'inc/guest.inc.php';
+    case 'gbook':
+        include 'inc/gbook.inc.php';
         break;
     case 'log':
         include 'inc/view-log.inc.php';
